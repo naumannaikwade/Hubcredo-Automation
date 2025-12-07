7d3b4309b45380ffaffbbe5f539fffa2b114c953
naumannaikwade <naumannaikwade25@gmail.com>
2025-12-07 06:59:47 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -23,7 +23,7 @@ const Login = ({ setIsAuthenticated }) => {
     setLoading(true);
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post('https://hubcredobackend.onrender.com/api/auth/login', {
         email: formData.email,
         password: formData.password
       }, {
