4a0b532770826ffa6990c70cde37f7469b838c55
naumannaikwade <naumannaikwade25@gmail.com>
2025-12-07 06:51:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,15 @@ app.use((err, req, res, next) => {
   });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
